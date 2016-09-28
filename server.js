690b7790f39a4dc42a354923d725e40d31015a75
shemoreira <leonarrangel@gmail.com>
2016-09-28 10:20:22 -0300

    Adicionado configuraçao para heroku

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname));
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(port, ip);
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
